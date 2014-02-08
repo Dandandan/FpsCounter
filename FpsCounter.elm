f52331412e1ad8595f3b7f439eff6c280591e455
DanDanDan <danielheres@gmail.com>
2014-02-08 01:05:53 +0100

    0.1.4

--- FpsCounter.elm
+++ FpsCounter.elm
@@ -2,19 +2,19 @@ module FpsCounter (fpsCounter, createFpsCounter) where
 
 {-| A frame rate counter chart for your Elm program.
 
+@docs fpsCounter, createFpsCounter
 -}
 
 average : [Float] -> Float
 average xs = sum xs / toFloat (length xs)
 
-sixty = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,26,28,29,30
-        ,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
+sixty = [1..60]
 
-{- Measures fps of the program and creates a real time updating element -}
+{-| Measures fps of the program and creates a real time updating element -}
 fpsCounter : Int -> Int -> Signal Element
 fpsCounter f n = createFpsCounter f <~ fps 3 `sampleOn` remember n (fps f)
 
-{- Creates a fps counter 
+{-| Creates a fps counter
 
 * First argument is frames per second
 * Second argument is a list of the time between frames in milliseconds
